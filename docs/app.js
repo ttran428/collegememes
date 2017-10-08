af732913c9ccfb2aeee0afd0c9e659c3af4f1d7a
zacool64 <zaristei@gmail.com>
2017-10-07 18:12:08 -0700

    a lot2

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs")
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
     res.render("index");
 })
