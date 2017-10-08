eb4cbfc74fff7dd8fbefe452e5d94a4590f9b79e
zacool64 <zaristei@gmail.com>
2017-10-07 18:08:53 -0700

    a lot

--- docs/app.js
+++ docs/app.js
@@ -7,6 +7,9 @@ app.get("/", function(req, res) {
     res.render("index");
 })
 
+app.get("/home", function(req, res) {
+    res.send("HEY");
+})
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Memes are loading...")
 })
